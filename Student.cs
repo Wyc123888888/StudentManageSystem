1f455d323454c21d8d7390758969b2383dd82401
WeiLLLL <128537249+WeiLLLL@users.noreply.github.com>
2023-11-02 22:39:37 -0400

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SMTIOnlineCourseRegistration.DAL;
+
+namespace SMTIOnlineCourseRegistration.BLL
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public List<Student> GetAllStudents()
+        {
+            return StudentDB.GetAllRecords();
+        }
+
+        public Student SearchStudentById(int userUserId)
+        {
+            return StudentDB.GetStudent(userUserId);
+        }
+    }
+}
---
+++ StudentDB.cs
@@ -0,0 +1,64 @@
+﻿using SMTIOnlineCourseRegistration.BLL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using SMTIOnlineCourseRegistration.BLL;
+
+namespace SMTIOnlineCourseRegistration.DAL
+{
+    public class StudentDB
+    {
+        public static List<Student> GetAllRecords()
+        {
+            List<Student> listS = new List<Student>();
+            using (SqlConnection conn = UtilityDB.GetDBConnection())
+            {
+                SqlCommand cmdAllStudent = new SqlCommand("SELECT * FROM Students", conn);
+                SqlDataReader dReader = cmdAllStudent.ExecuteReader();
+                Student student;
+                while (dReader.Read())
+                {
+                    student = new Student
+                    {
+                        StudentId = Convert.ToInt32(dReader["StudentId"]),
+                        FirstName = dReader["FirstName"].ToString(),
+                        LastName = dReader["LastName"].ToString(),
+                        Email = dReader["Email"].ToString()
+                    };
+                    listS.Add(student);
+                }
+            }
+
+            return listS;
+        }
+
+        public static Student GetStudent(int sId)
+        {
+            using (SqlConnection conn = UtilityDB.GetDBConnection())
+            {
+                SqlCommand cmdStudent = new SqlCommand();
+                cmdStudent.CommandText = "SELECT * FROM Students " +
+                                         "WHERE StudentId = @StudentId";
+                cmdStudent.Connection = conn;
+                cmdStudent.Parameters.AddWithValue("@StudentId", sId);
+                SqlDataReader dReader = cmdStudent.ExecuteReader();
+                Student s = new Student();
+                if (dReader.Read())
+                {
+                    s.StudentId = Convert.ToInt32(dReader["StudentId"]);
+                    s.FirstName = dReader["FirstName"].ToString();
+                    s.LastName = dReader["LastName"].ToString();
+                    s.Email = dReader["Email"].ToString();
+                }
+                else
+                {
+                    s = null;
+                }
+                return s;
+            }
+        }
+
+    }
+}
